053698d34b5dd55fdaf9137c80bf8c054a9eb939
Uwe Gehring <uwe@imap.cc>
2020-06-04 15:45:48 +0200

    Initial version

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(vhostmanage):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="vhostmanage" command="/usr/bin/vhostmanage"
--- debian/vhostmanage.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the vhostmanage package
-#
-0 4	* * *	root	[ -x /usr/bin/vhostmanage_maintenance ] && /usr/bin/vhostmanage_maintenance
