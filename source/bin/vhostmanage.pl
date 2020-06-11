be720b13b4c67fadd125917c030123b89e1deda2
Uwe Gehring <uwe@imap.cc>
2020-06-11 13:48:37 +0200

    Fixed: License

--- source/bin/vhostmanage.pl
+++ source/bin/vhostmanage.pl
@@ -1,8 +1,20 @@
 #!/usr/bin/perl -w
 #
-# vhostmanage by Uwe Gehring <adspectus@fastmail.com> is based upon:
-# a2enmod by Stefan Fritsch <sf@debian.org>
+# Copyright 2020 Uwe Gehring <adspectus@fastmail.com>
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Credits: vhostmanage is based upon a2enmod by Stefan Fritsch <sf@debian.org>
 
 use strict;
 use Cwd 'realpath';
